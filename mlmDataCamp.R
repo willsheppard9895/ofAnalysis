6e9fe53ec2a50c0010ab87ed6c0b45e297dfaf4f
William Sheppard <cn13ws@leeds.ac.uk>
2022-10-11 11:06:44 +0100

    upload
    upload

---
+++ mlmDataCamp.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(stats)
+library(lme4)
+
+# read in data
+ofData <- read_csv("../cleanData/ofData.csv")
+
+#  build a regression model
+mod <- lm(absAngError ~ condition, data = ofData)
+summary(mod)
+mod
+coef_plot <- tidy(mod)
+
+
+ggplot(mod, aes(x = condition, y = absAngError))+
+  geom_point(alpha = .01)+
+  theme_minimal()+
+  geom_abline(intercept = mod$coefficients[1],
+              slope = mod$coefficients[2])
+
+# buidl a mixed effecs model
+lmer1 <- lmer(absAngError ~ condition * contrast + (1|Participant.Private.ID), data = ofData)
+summary(lmer1)
+tidy(lmer1)
